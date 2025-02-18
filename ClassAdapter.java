62de4fe6885f022413b839dcc17bb2d87ed5c4a0
xPenguinx <55804219+VPatel5@users.noreply.github.com>
2025-02-17 23:20:09 -0500

    use super class

--- ClassAdapter.java
+++ ClassAdapter.java
@@ -4,38 +4,37 @@ import java.util.List;
 
 public class ClassAdapter<E extends Comparable<? super E>> extends
 SortedList<E> implements SortedSet<E> {
-	private final SortedList<E> internalSortedList = new SortedList<>();
 
 	@Override
 	public void add(E e)
 	{
-		if (!internalSortedList.contains(e))
+		if (!contains(e))
 		{
-			internalSortedList.add(e);
+			super.add(e);
 		}
 	}
 
 	@Override
 	public boolean contains(E e)
 	{
-		return internalSortedList.contains(e);
+		return super.contains(e);
 	}
 
 	@Override
 	public boolean remove(E e)
 	{
-		return internalSortedList.remove(e);
+		return super.remove(e);
 	}
 
 	@Override
 	public List<E> asList()
 	{
-		return internalSortedList.asList();
+		return super.asList();
 	}
 
 	@Override
 	public int size()
 	{
-		return internalSortedList.size();
+		return super.size();
 	}
 }
