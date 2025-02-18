ba9d6dacacef68cea0bc069be093dfcae386aba8
Ashley <ayu62@binghamton.edu>
2025-02-17 23:37:54 -0500

    objectadapter

--- ObjectAdapter.java
+++ ObjectAdapter.java
@@ -13,26 +13,34 @@ public class ObjectAdapter<E extends Comparable<? super E>> implements SortedSet
 	@Override
 	public void add(E e) {
 		// add e to adaptee if e is not in the adaptee already
-		// otherwise do nothing
+		// otherwise do nothing)
+		if(!adaptee.contains(e)){
+			adaptee.add(e);
+		}
 	}
 	@Override
 	public boolean contains(E e) {
 		// delegate to adaptee
+		return adaptee.contains(e);
 	}
 	@Override
 	public boolean remove(E e) {
 		// delegate to adaptee
+		return adaptee.remove(e);
 	}
 	@Override
 	public String toString() {
 		// delegate to adaptee
+		return adaptee.toString();
 	}
 	@Override
 	public List<E> asList() {
 		// delegate to adaptee
+		return adaptee.asList();
 	}
 	@Override
 	public int size() {
 		// delegate to adaptee
+		return adaptee.size();
 	}
 }
